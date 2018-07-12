ac0f4b9b66300d86174a6d7a64ee9e30bb1705a9
Pablo Baeza <pablo.baeza@crg.eu>
2018-07-12 16:41:49 +0200

    added analysis of small-scale RT-PCR assays in three different cell lines

---
+++ 007_Three_Cell_Lines/001_G44A_in_3_cell_lines.R
@@ -0,0 +1,353 @@
+
+
+############
+## HEK293 ##
+############
+
+# gel electrophoresis
+With.44G <- c(99.1,
+              99,
+              98.2,
+              94.3,
+              94.3,
+              15.1,
+              44)
+
+With.44A <- c(98.3,
+              97.9,
+              95.5,
+              73.5,
+              27.2,
+              3.5,
+              3.4)
+
+With.44G.SD <- c(0.7,
+                 0.2,
+                 0.2,
+                 0.5,
+                 0.5,
+                 0.9,
+                 1.5)
+
+With.44A.SD <- c(0.2,
+                 0.8,
+                 0.3,
+                 0.7,
+                 1.6,
+                 1.5,
+                 1)
+
+Combined.SD <- sqrt(With.44G.SD^2 + With.44A.SD^2)
+
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(0,1, lwd = 3, col = "gray90")
+segments(x0 = With.44G,
+         x1 = With.44G,
+         y0 = With.44A+With.44A.SD,
+         y1 = With.44A-With.44A.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = With.44G+With.44G.SD,
+         x1 = With.44G-With.44G.SD,
+         y0 = With.44A,
+         y1 = With.44A,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(With.44G,
+     With.44A,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     pch = 19,
+     las = 1,
+     xlab = "44G",
+     ylab = "44A")
+
+
+
+
+
+
+
+
+
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(h=0, lwd = 3, col = "gray90")
+segments(x0 = With.44G,
+         x1 = With.44G,
+         y0 = With.44A-With.44G+Combined.SD,
+         y1 = With.44A-With.44G-Combined.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = With.44G+With.44G.SD,
+         x1 = With.44G-With.44G.SD,
+         y0 = With.44A-With.44G,
+         y1 = With.44A-With.44G,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(With.44G,With.44A-With.44G,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     pch = 19,
+     las = 1,
+     xlab = "With.44G",
+     ylab = "With.44A - With.44G")
+
+
+
+
+
+
+
+
+
+
+##########
+## HeLa ##
+##########
+
+
+
+
+# gel electrophoresis
+With.44G <- c(98.4,
+              95.3,
+              97.4,
+              97.6,
+              97.9,
+              55.1,
+              82.6)
+
+With.44A <- c(98,
+              96.3,
+              95.2,
+              89.6,
+              59.3,
+              8.7,
+              11.1)
+
+With.44G.SD <- c(0.5,
+                 1.6,
+                 0.4,
+                 0.4,
+                 0.4,
+                 1.4,
+                 0.8)
+
+With.44A.SD <- c(0.5,
+                 0.4,
+                 0.7,
+                 6.3,
+                 0.9,
+                 2.4,
+                 1.9)
+
+Combined.SD <- sqrt(With.44G.SD^2 + With.44A.SD^2)
+
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(0,1, lwd = 3, col = "gray90")
+segments(x0 = With.44G,
+         x1 = With.44G,
+         y0 = With.44A+With.44A.SD,
+         y1 = With.44A-With.44A.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = With.44G+With.44G.SD,
+         x1 = With.44G-With.44G.SD,
+         y0 = With.44A,
+         y1 = With.44A,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(With.44G,
+     With.44A,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     pch = 19,
+     las = 1,
+     xlab = "44G",
+     ylab = "44A")
+
+
+
+
+
+
+
+
+
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(h=0, lwd = 3, col = "gray90")
+segments(x0 = With.44G,
+         x1 = With.44G,
+         y0 = With.44A-With.44G+Combined.SD,
+         y1 = With.44A-With.44G-Combined.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = With.44G+With.44G.SD,
+         x1 = With.44G-With.44G.SD,
+         y0 = With.44A-With.44G,
+         y1 = With.44A-With.44G,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(With.44G,With.44A-With.44G,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     pch = 19,
+     las = 1,
+     xlab = "With.44G",
+     ylab = "With.44A - With.44G")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###########
+## COS-7 ##
+###########
+
+
+
+
+
+# gel electrophoresis
+With.44G <- c(97.5,
+              96.9,
+              100,
+              98.6,
+              98.2,
+              17.8,
+              48)
+
+With.44A <- c(98.5,
+              95.3,
+              98.2,
+              84.3,
+              32.3,
+              1.6,
+              0.2)
+
+With.44G.SD <- c(1.4,
+                 1,
+                 0.1,
+                 0.6,
+                 1.6,
+                 0.7,
+                 1.5)
+
+With.44A.SD <- c(1.3,
+                 0.4,
+                 1.1,
+                 2.4,
+                 1.5,
+                 0.2,
+                 0.6)
+
+Combined.SD <- sqrt(With.44G.SD^2 + With.44A.SD^2)
+
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(0,1, lwd = 3, col = "gray90")
+segments(x0 = With.44G,
+         x1 = With.44G,
+         y0 = With.44A+With.44A.SD,
+         y1 = With.44A-With.44A.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = With.44G+With.44G.SD,
+         x1 = With.44G-With.44G.SD,
+         y0 = With.44A,
+         y1 = With.44A,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(With.44G,
+     With.44A,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     pch = 19,
+     las = 1,
+     xlab = "44G",
+     ylab = "44A")
+
+
+
+
+
+
+
+
+
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(h=0, lwd = 3, col = "gray90")
+segments(x0 = With.44G,
+         x1 = With.44G,
+         y0 = With.44A-With.44G+Combined.SD,
+         y1 = With.44A-With.44G-Combined.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = With.44G+With.44G.SD,
+         x1 = With.44G-With.44G.SD,
+         y0 = With.44A-With.44G,
+         y1 = With.44A-With.44G,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(With.44G,With.44A-With.44G,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     pch = 19,
+     las = 1,
+     xlab = "With.44G",
+     ylab = "With.44A - With.44G")
+
+
+
---
+++ 007_Three_Cell_Lines/002_cell_type_scaling.md
@@ -0,0 +1,296 @@
+# Scaling of effects due to cell type change
+
+In our paper we describe how the effect of changing the cell type where a splicing event taes place affects scales with the starting levels of exon inclusion. In this document I describe the R code used to visualise these results.
+
+
+## 1. From HEK293 to HeLa
+
+I first manually input the RT-PCR results into R:
+
+```r
+# RT-PCR results for HEK293
+Hek <- c(98.3,
+         99.1,
+         97.9,
+         99,
+         95.5,
+         98.2,
+         73.5,
+         94.3,
+         27.2,
+         94.3,
+         3.5,
+         15.1,
+         3.4,
+         44)
+Hek.SD <- c(0.2,
+            0.7,
+            0.8,
+            0.2,
+            0.3,
+            0.2,
+            0.7,
+            0.5,
+            1.6,
+            0.5,
+            1.5,
+            0.9,
+            1,
+            1.5)
+
+# RT-PCR results for HeLa
+Hela <- c(98,
+          98.4,
+          96.3,
+          95.3,
+          95.2,
+          97.4,
+          89.6,
+          97.6,
+          59.3,
+          97.9,
+          8.7,
+          55.1,
+          11.1,
+          82.6)
+Hela.SD <- c(0.5,
+             0.5,
+             0.4,
+             1.6,
+             0.7,
+             0.4,
+             6.3,
+             0.4,
+             0.9,
+             0.4,
+             2.4,
+             1.4,
+             1.9,
+             0.8)
+
+# propagated error for delta PSI
+Combined.SD <- sqrt(Hela.SD^2 + Hek.SD^2)
+```
+We can now draw the HeLa (final PSI) vs HEK293 (starting PSI) plot:
+
+```r
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(0,1, lwd = 3, col = "gray90")
+segments(x0 = Hek,
+         x1 = Hek,
+         y0 = Hela+Hela.SD,
+         y1 = Hela-Hela.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = Hek+Hek.SD,
+         x1 = Hek-Hek.SD,
+         y0 = Hela,
+         y1 = Hela,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(Hek,
+     Hela,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     pch = 19,
+     las = 1,
+     xlab = "HEK293",
+     ylab = "HeLa")
+
+# library for orthogonal least squares
+library(onls)
+
+# find the parameter A that best explains the data
+Scaling.Effect <- onls(Hela ~ (100*A*Hek)/(100-Hek+A*Hek), start = list(A=1) )
+
+# and draw the curve
+Line.X <- seq(0,100,1)
+Line.Y <- predict(object = Scaling.Effect,
+                  newdata = data.frame(Hek = Line.X))
+lines(x = Line.X,
+      y = Line.Y,
+      col = "orange",
+      lwd = 2)
+```
+<p align="center">
+  <img width = 450 height = 450 src="Figures/Hek_to_Hela_1.png">
+  <br> Figure 4H
+</p>
+
+And the delta PSI plot:
+
+```r
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(h=0, lwd = 3, col = "gray90")
+segments(x0 = Hek,
+         x1 = Hek,
+         y0 = Hela-Hek+Combined.SD,
+         y1 = Hela-Hek-Combined.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = Hek+Hek.SD,
+         x1 = Hek-Hek.SD,
+         y0 = Hela-Hek,
+         y1 = Hela-Hek,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(Hek,Hela-Hek,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     pch = 19,
+     las = 1,
+     xlab = "HEK293",
+     ylab = "HeLa - HEK293")
+
+lines(x = Line.X,
+      y = Line.Y-Line.X,
+      col = "orange",
+      lwd = 2)
+```
+<p align="center">
+  <img width = 450 height = 450 src="Figures/Hek_to_Hela_2.png">
+  <br> Figure 4H
+</p>
+
+## 2. From HeLa to COS-7
+
+Input the COS-7 data:
+
+```r
+# RT-PCR results for COS-7
+Cos7 <- c(98.5,
+          97.5,
+          95.3,
+          96.9,
+          98.2,
+          100,
+          84.3,
+          98.6,
+          32.3,
+          98.2,
+          1.6,
+          17.8,
+          0.2,
+          48)
+Cos7.SD <- c(1.3,
+             1.4,
+             0.4,
+             1,
+             1.1,
+             0.1,
+             2.4,
+             0.6,
+             1.5,
+             1.6,
+             0.2,
+             0.7,
+             0.6,
+             1.5)
+
+# propagated error for delta PSI
+Combined.SD <- sqrt(Hela.SD^2 + Cos7.SD^2)
+```
+Plot HeLa vs COS-7 PSI:
+
+```r
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(0,1, lwd = 3, col = "gray90")
+segments(x0 = Hela,
+         x1 = Hela,
+         y0 = Cos7+Cos7.SD,
+         y1 = Cos7-Cos7.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = Hela+Hela.SD,
+         x1 = Hela-Hela.SD,
+         y0 = Cos7,
+         y1 = Cos7,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(Hela,
+     Cos7,
+     xlim = c(0,100),
+     ylim = c(0,100),
+     pch = 19,
+     las = 1,
+     xlab = "HeLa",
+     ylab = "COS-7")
+
+# find the parameter A that best explains the data
+Scaling.Effect <- onls(Cos7 ~ (100*A*Hela)/(100-Hela+A*Hela), start = list(A=1) )
+
+# and draw the curve
+Line.X <- seq(0,100,1)
+Line.Y <- predict(object = Scaling.Effect,
+                  newdata = data.frame(Hela = Line.X))
+lines(x = Line.X,
+      y = Line.Y,
+      col = "orange",
+      lwd = 2)
+```
+<p align="center">
+  <img width = 450 height = 450 src="Figures/Hela_to_Cos_1.png">
+  <br> Figure 4H
+</p>
+
+```r
+par(pty="s")
+plot(NULL,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     xlab = "",
+     ylab = "",
+     axes = F)
+abline(h=0, lwd = 3, col = "gray90")
+segments(x0 = Hela,
+         x1 = Hela,
+         y0 = Cos7-Hela+Combined.SD,
+         y1 = Cos7-Hela-Combined.SD,
+         col = "gray70",
+         lwd = 1.5)
+segments(x0 = Hela+Hela.SD,
+         x1 = Hela-Hela.SD,
+         y0 = Cos7-Hela,
+         y1 = Cos7-Hela,
+         col = "gray70",
+         lwd = 1.5)
+par(new=T)
+plot(Hela,Cos7-Hela,
+     xlim = c(0,100),
+     ylim = c(-100,100),
+     pch = 19,
+     las = 1,
+     xlab = "HeLa",
+     ylab = "COS-7 - HeLa")
+
+lines(x = Line.X,
+      y = Line.Y-Line.X,
+      col = "orange",
+      lwd = 2)
+```
+
+<p align="center">
+  <img width = 450 height = 450 src="Figures/Hela_to_Cos_2.png">
+  <br> Figure 4H
+</p>
