28c5df851cf3469b8ce25713f596cbba3c52fb7a
Pablo <7280608+pablo-baeza@users.noreply.github.com>
2019-02-11 12:35:37 +0100

    awk typo

--- 011_Splice_Site_Usage_Across_Conditions/002_HepG2_vs_HUVEC/build_tables.sh
+++ 011_Splice_Site_Usage_Across_Conditions/002_HepG2_vs_HUVEC/build_tables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # alternative 5' splice sites
-awk 'NR==1 {print}; $2 ~ /^HsaALTD / {print $0}' Huvec_HepG2_TABLE.txt >> Data/Huvec_HepG2_TABLE_ALTD.txt
+awk 'NR==1 {print}; $2 ~ /^HsaALTD/ {print $0}' Huvec_HepG2_TABLE.txt >> Data/Huvec_HepG2_TABLE_ALTD.txt
 
 # alternative 3' splice sites
-awk 'NR==1 {print}; $2 ~ /^HsaALTA / {print $0}' Huvec_HepG2_TABLE.txt >> Data/Huvec_HepG2_TABLE_ALTA.txt
+awk 'NR==1 {print}; $2 ~ /^HsaALTA/ {print $0}' Huvec_HepG2_TABLE.txt >> Data/Huvec_HepG2_TABLE_ALTA.txt
