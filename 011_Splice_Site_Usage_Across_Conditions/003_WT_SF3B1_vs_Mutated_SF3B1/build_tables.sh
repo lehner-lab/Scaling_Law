3d7d95c0352bba1b2cb54fe85f8ed1e7c2775987
Pablo <7280608+pablo-baeza@users.noreply.github.com>
2019-02-11 12:20:57 +0100

    awk code typo

--- 011_Splice_Site_Usage_Across_Conditions/003_WT_SF3B1_vs_Mutated_SF3B1/build_tables.sh
+++ 011_Splice_Site_Usage_Across_Conditions/003_WT_SF3B1_vs_Mutated_SF3B1/build_tables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # alternative 5' splice sites
-awk 'NR==1 {print}; $2 ~ /^HsaALTD / {print $0}' SF3B1wt_SF3B1mut_TABLE.txt >> Data/SF3B1wt_SF3B1mut_TABLE_ALTD.txt
+awk 'NR==1 {print}; $2 ~ /^HsaALTD/ {print $0}' SF3B1wt_SF3B1mut_TABLE.txt >> Data/SF3B1wt_SF3B1mut_TABLE_ALTD.txt
 
 # alternative 3' splice sites
-awk 'NR==1 {print}; $2 ~ /^HsaALTA / {print $0}' SF3B1wt_SF3B1mut_TABLE.txt >> Data/SF3B1wt_SF3B1mut_TABLE_ALTA.txt
+awk 'NR==1 {print}; $2 ~ /^HsaALTA/ {print $0}' SF3B1wt_SF3B1mut_TABLE.txt >> Data/SF3B1wt_SF3B1mut_TABLE_ALTA.txt
